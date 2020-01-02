4bda4ada6492316d74d8e413d46e7092c75c52a4
fabianboesiger <boesiger.fabian@outlook.com>
2020-01-02 16:06:47 +0100

    changes

--- src/database/store.rs
+++ src/database/store.rs
@@ -1,7 +1,7 @@
 use super::Serialize;
 
-pub trait Store: Serialize + Default {
-    type ID: Serialize + std::fmt::Display;
+pub trait Store: Serialize + Default + Send + Sync {
+    type ID: Serialize + std::fmt::Display + Send + Sync;
 
     fn name() -> &'static str;
     fn id(&self) -> Self::ID;
